79a235209c155301856769f2470ba05270c39bef
Beto-cpu <beto.munguia.lara@gmail.com>
2022-05-08 11:24:24 -0700

    Harry Potter Practice modified

--- practicaHarry/src/practicaHarry/Character.java
+++ practicaHarry/src/practicaHarry/Character.java
@@ -82,18 +82,3 @@ public class Character {
     }
 }
 
-class CharacterTest{
-    public static void main(String[] args) {
-        Character harry = new Character("Harry James Potter", "Male", "Stag", "Gryffindor", "Half Blood", "Dementor");
-        Character hermione = new Character("Hermione Jean Granger", "Female", "Otter", "Gryffindor", "Muggle Born", "Failure");
-        Character bellatrix = new Character("Bellatrix Lestrange", "Female", null, "Slytherin", "Pure Blood", null);
-        Character nigini = new Character("Nagini", "Female", null, null, "Underbeing", null);
-        Character sirius = new Character("Sirius Black III", "Male", "Non corporeal", "Gryffindor", "Pure Blood", "Lord Boldemord");
-
-        System.out.println(harry);
-        System.out.println(hermione);
-        System.out.println(bellatrix);
-        System.out.println(nigini);
-        System.out.println(sirius);
-    }
-}
